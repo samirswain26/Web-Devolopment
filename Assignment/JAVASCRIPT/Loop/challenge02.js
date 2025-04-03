51a75bf928681bf9dff8651e7ce4509090093612
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-03 11:25:51 +0530

    challenge 2

---
+++ Assignment/JAVASCRIPT/Loop/challenge02.js
@@ -0,0 +1,10 @@
+// create a function that count the number of apples in the basket using a loop.
+
+function countApples(apples) {
+    let count = 0;
+   for (let i = 0; i < apples; i++) {
+     count++
+   }
+   return count;
+ }
+ 
