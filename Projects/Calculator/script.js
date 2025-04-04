045062ab38b923ae57b5c3880e0355113e26999f
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-04 10:05:59 +0530

    first phase over in js

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,11 +1,21 @@
 const dispalyDrc = document.getElementById("dispaly-src");
 const buttonkeys = document.getElementsByClassName("keys");
 const one = document.getElementById("one")
+const two = document.getElementById("two")
 
 one.addEventListener("click", ()=>{
     const value = one.innerText
     // dispalyDrc.innerText = value
     if(value === one.innerText){
+        dispalyDrc.innerText += value
+        console.log(value);
+        
+    }    
+})
+two.addEventListener("click", ()=>{
+    const value = two.innerText
+    // dispalyDrc.innerText = value
+    if(value === two.innerText){
         dispalyDrc.innerText += value
         console.log(value);
         
