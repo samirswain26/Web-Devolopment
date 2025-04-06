8620c53abc96591494a276678ea48b00b769df95
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-06 23:29:10 +0530

    Adding some extra features

--- Projects/Kanban Board/script.js
+++ Projects/Kanban Board/script.js
@@ -6,6 +6,25 @@ const dragable = document.getElementsByClassName("item")
 
 const allboards = document.querySelector("#progress-board")
 
+const container = document.getElementById("container")
+const addboard = document.getElementById("add-board")
+
+addboard.addEventListener("click", ()=>{
+    console.log("hello")
+    const input = prompt("Add Conatiner")
+    if(!input) return
+
+    const created = document.createElement("div")
+    created.innerText = input 
+    created.style.border = "2px solid black"
+    created.style.width = "200px"
+    created.style.height = "70vh"
+    created.style.borderRadius = "10px"
+    created.style.display = "flex"
+    created.style.fontFamily = " Segoe UI, Tahoma, Geneva, Verdana, sans-serif;"
+    created.style.padding = "10px"
+    container.appendChild(created)
+})
 allboards.addEventListener("click", ()=> {
     console.log("This div was clicked")
 })
