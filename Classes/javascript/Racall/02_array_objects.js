77630ef7295420a06a065e9b41f88e7bc8312bf2
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-15 21:37:27 +0530

    Array

---
+++ Classes/javascript/Racall/02_array_objects.js
@@ -0,0 +1,32 @@
+let Heros = ["Iron Man", "Spider Man", "Captain America", "Thor"]
+
+console.log(Heros[0]);
+console.log(Heros.length);
+
+Heros.push("Captain Marvel")
+// console.log(Heros);
+
+let del =  Heros.pop()
+console.log(del)        //The output is :- Del the array list except the push element.
+ 
+let index = Heros.indexOf("ThorR")
+console.log(index);
+
+if(index != -1){
+    Heros.splice(index, 1)
+}
+console.log(Heros)
+
+Heros.forEach((app, index)=>{
+    console.log(`${index + 1} : ${app}`)
+})
+
+
+let Dcheros = ["Super Mam", "Batman"]
+
+let allheros = Heros.concat(Dcheros)
+console.log(allheros);
+
+let newtype = [...Heros, "hulk"]
+console.log(newtype);
+
