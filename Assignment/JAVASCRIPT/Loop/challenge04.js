af64e6152b5302e28b69333f96c60e2e2b56a38d
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-18 08:38:26 +0530

    Used For loop

---
+++ Assignment/JAVASCRIPT/Loop/challenge04.js
@@ -0,0 +1,14 @@
+// Cretae a function that takes an array of water amount (in liters) for each bottle and add them up.The fumction should return the total amount of water you've filled in all the bottles.
+
+let water = [1,2,3,4]
+
+function totalWater(waterAmounts) {
+    let total = 0;
+
+    for (let i = 0; i < waterAmounts.length; i++){
+        total = total + waterAmounts[i]
+    }
+    return total
+}
+
+console.log(totalWater([1,2,3,4]));
