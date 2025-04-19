9a4fb7b3ee2e6a5f2f49d0d8fcf45a3ef9edbfe6
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-19 10:36:24 +0530

    tash 10

---
+++ Assignment/JAVASCRIPT/Array/challenge10.js
@@ -0,0 +1,7 @@
+//  Create a function that returns the number of movies in the movies list.
+
+function countMovies(movieList) {
+    return movieList.length
+}
+ 
+console.log(countMovies(["a", "b" ,"c"]));
