dd9be0371ecb1e0174d67b2b36a2e8bd4c521fa5
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-19 10:09:11 +0530

    tash 7

---
+++ Assignment/JAVASCRIPT/Array/challenge07.js
@@ -0,0 +1,9 @@
+// Create a function that finds the index of "phone" in the list of items and return the index.
+
+function findPhone(items) {
+    return items.indexOf("Phone")
+}
+
+let a = ["a", "b", "c", 'd',"e", "Phone"]
+let b = a.indexOf("Phone")
+console.log(b);
