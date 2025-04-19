f795cb66ef9991fc29488a42779340288b5246e9
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-19 09:32:26 +0530

    Task 2

---
+++ Assignment/JAVASCRIPT/Array/challenge02.js
@@ -0,0 +1,7 @@
+// create a function that removes the last candy from the jar and returns the uploaded jar.
+
+function eatCandy(candyJar) {
+    // Remove the last candy from the jar and return the updated jar
+    candyJar.pop()
+    return candyJar
+}
