da40e599ed4f43fb20958f40647be050f60095b0
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-19 09:27:39 +0530

    Task 1

---
+++ Assignment/JAVASCRIPT/Array/challenge01.js
@@ -0,0 +1,7 @@
+// Create a function that adds a new guest list and returns the updated list.
+
+function addGuest(guestList, newGuest) {
+    // Add the newGuest to guestList and return the updated list
+    guestList.push(newGuest);
+    return guestList
+}
