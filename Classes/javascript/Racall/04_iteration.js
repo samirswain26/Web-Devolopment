e79dd56aa983729eb0feeadda262279fbc133608
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-04-22 17:01:43 +0530

    Mathematical Coding Problems in interation

--- Classes/javascript/Racall/04_iteration.js
+++ Classes/javascript/Racall/04_iteration.js
@@ -16,8 +16,8 @@ let userActivity = [
     {user: "jeet", activitycount: 52},
 ]
 //Finding the mostActiveUser using reduce method.
-let activeuser = userActivity.reduce((accum, crntval)=> 
-crntval.activitycount > accum.activitycount ? crntval : accum
+let activeuser = userActivity.reduce((accum, currentvalue)=> 
+    currentvalue.activitycount > accum.activitycount ? currentvalue : accum
 )
 console.log(activeuser);
 
@@ -36,3 +36,57 @@ let lowstockitem = stackitems.filter((item) => {
 })
 // Filter method give an array in the output. 
 console.log(lowstockitem);
+
+
+
+let expenses = [
+    {description: "Groceries", amount: 50, category: "Food", Rate: 30},
+    {description: "Electricity Bill", amount: 100, category: "utilities",Rate: 300 },
+    {description: "Dinner", amount: 30, category: "Food", Rate: 1000},
+    {description: "Intenet Bill", amount: 50, category: "utilities", Rate: 590},
+    {description: "Book", amount: 50, category: "Study", Rate: 30}
+];
+
+let expenseReport = expenses.reduce((accum, curval) => {
+    accum[curval.category] += curval.amount
+    // accum[curval.category] += curval.Rate
+    return accum
+}, {Food: 0, utilities: 0, Study: 0})
+
+console.log("Expense Report", expenseReport);
+
+
+
+
+// Solve this problem in a sorted way according to the prioprity the task is completed or not.
+let tasks = [
+    {description: "Write Report", completed: false, priority: 2},
+    {description: "Write Code", completed: false, priority: 2},
+    {description: "Send mail", completed: true, priority: 9},
+    {description: "Prepare Presentation", completed: false, priority: 6},
+]
+
+let pendingSortedTasks = tasks
+    .filter((task) => {return !task.completed})
+    .sort((a, b) => a.priority - b.priority)   // Sorts them by increasing priority
+
+console.log(pendingSortedTasks);
+
+
+
+// Find the avergae movie ratings for each movie.
+let moviratings = [
+    {title: "Movie A", ratings: [4,5,3]},
+    {title: "Movie B", ratings: [5,5,4]},
+    {title: "Movie C", ratings: [3,4,2]},
+]
+
+ let averageratings = moviratings.map((add) => {
+    let total =  add.ratings.reduce((accum, curval) => accum + curval,  0 )
+    let average = total / add.ratings.length
+    add.ratings = average
+    return add
+    return {title: add.title, averageratings: average.toFixed(1) }
+ })
+ console.log(averageratings);
+ 
