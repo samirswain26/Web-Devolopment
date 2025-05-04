5803b26f5eaf1a3229f34394c0473d7ec6674e70
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-05-04 08:50:05 +0530

    Login completed

--- Projects/Fullstack/controller/user.controller.js
+++ Projects/Fullstack/controller/user.controller.js
@@ -53,7 +53,7 @@ const registeruser = async (req,res) => {
         const transporter = nodemailer.createTransport({
             host: process.env.MAILTRAP_HOST,
             port: process.env.MAILTRAP_PORT,
-            secure: false, // true for port 465, false for other ports
+            secure: false, 
             auth: {
               user: process.env.MAILTRAP_USERNAME,
               pass: process.env.MAILTRAP_PASSWORD,
--- Projects/Fullstack/model/User.model.js
+++ Projects/Fullstack/model/User.model.js
@@ -27,6 +27,7 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 })
 
+// Used Hook (Pre) is used to to protetct the password
 userSchema.pre("save", async function(next){
     if(this.isModified("password")){
         this.password = await bcrypt.hash(this.password, 10)
