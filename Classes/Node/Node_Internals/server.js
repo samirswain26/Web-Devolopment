6b0bb7567030ea34dccd1cb4bf600c43b5cb70e3
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-05-13 23:29:25 +0530

    Step 1 for createing EXPRESS

--- Classes/Node/Node_Internals/server.js
+++ Classes/Node/Node_Internals/server.js
@@ -1,1 +1,16 @@
-const express = require("express")
+const http = require("http")
+const url = require("url")
+
+const routes = {
+    GET: {},
+    POST:{}
+}
+
+function handler(req,res){
+    switch(req.url){
+        case http.get : {
+            
+        }
+    }
+    
+}
