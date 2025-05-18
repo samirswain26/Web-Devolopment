c208cc173d05a95f460e71486e73d9128daa0dff
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-05-19 02:11:35 +0530

    All Boilerplate code Ready

--- Projects/Task-Manager/src/app.js
+++ Projects/Task-Manager/src/app.js
@@ -2,4 +2,10 @@ import express from "express"
 
 const app = express()
 
+// router imports
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+import { healthCheck } from "./controllers/healthcheck.controllers.js";
+
+app.use("/api/v1/healthcheck", healthCheck)
+
 export default app;
