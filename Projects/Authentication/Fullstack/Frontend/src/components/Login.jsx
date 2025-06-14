2ced87a5ca38659b19af6b12906906d3b30f4f9f
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-06-14 09:55:26 +0530

    User logged In  Sucessfully through Frontend Client

--- Projects/Authentication/Fullstack/Frontend/src/components/Login.jsx
+++ Projects/Authentication/Fullstack/Frontend/src/components/Login.jsx
@@ -1,8 +1,62 @@
+import { useState } from "react"
+import apiClient from "../../service/apiClient"
+
+
 function Login () {
+    const [email, setEmail] = useState("")
+    const [ password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        setError("")
+
+        try {
+            console.log("Tyring to login");
+            const data = await apiClient.login( email, password)
+            console.log(`login data: `, data)
+
+        } catch (error) {
+            error.message
+            console.log(error)
+        }
+        
+    }
+
     return (
         <div>
             <h1>Welcome to login page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <form onSubmit={handleSubmit}>
+                <div className="Email">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                    type="text"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+                </div>
+                <div className="password">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                    type="text"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                </div>
+                <button
+                type="Submit"
+                disabled = {loading}
+                >{loading ? 'login.....' : 'Login'}
+                </button>
+            </form>
         </div>
     )
 }
--- Projects/Authentication/Fullstack/Frontend/src/components/Signup.jsx
+++ Projects/Authentication/Fullstack/Frontend/src/components/Signup.jsx
@@ -3,7 +3,7 @@ import apiClient from "../../service/apiClient"
 
 
 function Signup () {
-const [name, setName] = useState("")
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [ password, setPassword] = useState("")
     const [loading, setLoading] = useState(false)
